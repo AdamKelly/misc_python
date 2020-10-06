6d1a1facbd94c89b61a51eacb31621fcebfbe939
Adam Kelly <adam.kelly@live.ie>
2020-10-06 19:42:16 +0100

    Added for loop to iterate over the data

--- astronauts_in_space.py
+++ astronauts_in_space.py
@@ -1,6 +1,27 @@
-# This script will return the amount of people currently in space
+# This script will return the amount of astronauts currently in space
 
 import requests
 
-people = requests.get('http://api.open-notify.org/astros.json')
-print(people.text)
+data = requests.get('http://api.open-notify.org/astros.json')
+
+json_data = data.json()
+
+amount = json_data['number']
+print(f"There are {amount} people in space right now.")
+
+for index, key in enumerate(json_data):
+    if index == 1:
+        for values in json_data[key]:
+            print(values['name'])
+
+
+
+
+
+
+
+
+
+
+
+
